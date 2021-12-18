f2647cd0b6e1371f8747690e5a6d4c9c53f26a18
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2021-12-17 23:52:04 -0500

    Added a Second Toggle Button

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -18,6 +18,22 @@ $(document).ready(function() {
     }
   });
 
-  
+    // toggle button for getting to the top of the screen
+    const buttonToTriggerScroll = '.scroll-to-top button';
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 20) {
+        $(buttonToTriggerScroll).fadeIn();
+      } else {
+        $(buttonToTriggerScroll).fadeOut();
+      }
+    });
+  // get to the top by clicking the toggle button
+    $(buttonToTriggerScroll).click(function() {
+      $(window).scrollTop(0);
+      if ($('.new-tweet').is(':hidden')) {
+        $('.new-tweet').show();
+      }
+      $('textarea').focus();
+    });
 
 })
