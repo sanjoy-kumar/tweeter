b312476ea3062b3cc532dd016ab7a751c690e24f
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2021-12-17 23:36:21 -0500

    Responsive Design Completed.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 
 const createTweetElement = tweetObj => {
   //responsible for returning a tweet <article>
@@ -60,15 +35,104 @@ const createTweetElement = tweetObj => {
   `;
   return newTweet;
 };
-
-
-const renderTweets = function(tweets) {
+const renderTweets = arrayOfTweetObj => {
   //Need to prepend (to see latest tweets first) to .tweets-container
-  for (let obj of tweets) {
+  for (let obj of arrayOfTweetObj) {
     $('.tweet-container').prepend(createTweetElement(obj));
   }
 };
 
-renderTweets(data);
+const renderLastTweet = arrayOfTweetObj => {
+  const lastTweet = arrayOfTweetObj[arrayOfTweetObj.length - 1];
+  $('.tweet-container').prepend(createTweetElement(lastTweet));
+};
 
+const ajaxPost = (url, data, callback) => {
+  $.post(url, data, callback);
+};
+
+const loadTweets = queryString => {
+  let text = '';
+  for (let index in queryString) {
+    //text= is 5 chars long
+    if (index > 4) {
+      text += queryString[index];
+    }
+  }
+  return decodeURIComponent(text);
+};
+
+const resetErrorMessage = violation => {
+  if (violation === 'over count') {
+    $(".error-message").hide();
+    $(".error-message").empty();
+    $(".error-message").append("<p>Opps!!! Your tweet is too long!</p>");
+    $(".error-message").slideDown("slow");
+    $('textarea').focus();
+  } else if (violation === 'empty') {
+    $(".error-message").hide();
+    $(".error-message").empty();
+    $(".error-message").append("<p>Please type your tweet...</p>");
+    $(".error-message").slideDown("slow");
+    $('textarea').focus();
+  } else {
+    $(".error-message").hide();
+    $(".error-message").empty();
+    $('textarea').focus();
+  }
+};
+
+// re-size tweet-textbox automatically while typing
+$(document).ready(function() {
+  $('textarea').on('input', function() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+  });
+  //get tweets
+  $.get('/tweets', renderTweets);
+
+  // new tweet form set as hidden
+  $('.new-tweet').hide();
+
+  // toggle write new tweetbutton
+  $('.write').click(function(event) {
+    $('.new-tweet').slideToggle('slow');
+    $('textarea').focus();
+  });
+
+  // button clicked = form submitted
+  $('.submit-and-display button').click(function(event) {
+    event.preventDefault();
+    const data = $('form').serialize();
+    const dataLength = loadTweets(data).length;
+
+    if (dataLength > 140) {
+      resetErrorMessage('over count');
+    } else if (dataLength === 0) {
+      resetErrorMessage('empty');
+    } else {
+      resetErrorMessage();
+      ajaxPost('/tweets', data, function() {
+        //Get the tweet that was just posted
+        $.get('/tweets', renderLastTweet);
+        // clear the box after tweets are posted
+        $('textarea').val("");
+      });
+      //Reset the character counter to 140 after submitting the tweet
+      $(this)
+        .closest(".new-tweet")
+        .find(".counter")
+        .removeClass("negative-count")
+        .text(140);
+    }
+  });
+});
+
+
+// helper function, can be moved to additional file "helpers.js" in future
+const escape = function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
